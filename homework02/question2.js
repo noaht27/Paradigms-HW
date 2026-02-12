80cd68f04a208d750c828d6bc397377ff03d50c4
Noah Thompson <nthomps7@nd.edu>
2026-02-11 20:34:47 -0500

    question2.js

--- homework02/question2.js
+++ homework02/question2.js
@@ -129,21 +129,130 @@ function query4(defects){
     // find most defective component
     let defect_components = {};
 
+    // getting defective component counts for the defect_components object
     defects.forEach(defect => {
         defect_components[defect.component] = (defect_components[defect.component] || 0) + 1;
     });
-    console.log(defect_components);
-    return /*...*/;    
+
+    let maxVal = 0;
+    let currKey = "";
+    // iterating through defect_components object and tracking maximum value of defects along with the key
+    for (const key in defect_components){
+        if (maxVal < defect_components[key]){
+            maxVal = defect_components[key];
+            currKey = key;
+        }
+    }
+    console.log(currKey);
+
+    return String(currKey);    
 }
 
 function query5(defects){
-    /* Your implementation here */
-    return /*...*/;    
+    // username of developer who fixed most documentation defects
+    // track developer who fixed and their count
+
+    let names = {};
+    let user_tracker = {};
+    // developer who fixed the most documentation defects (status === "RESOLVED" && resolution === "FIXED" && component === "Documentation"
+    defects.forEach(defect => {
+        if (defect.status === "RESOLVED" && defect.resolution === "FIXED" && defect.component === "Documentation"){
+            user_tracker[defect.fixed_by_username] = (user_tracker[defect.fixed_by_username] || 0) + 1;
+
+            let realname = defect.fixed_by_real_name || defect.fixed_by_username;
+            names[defect.fixed_by_username] = realname;
+        }
+    });
+
+    // since giving preference to alphabetic order, want to sort by keys
+    const sortedKeys = Object.keys(user_tracker).sort();
+
+    let maxVal = 0;
+    let currKey = "";
+    // iterating through defect_components object and tracking maximum value of defects along with the key
+    for (let i = 0; i < sortedKeys.length; i++){
+        // this accounts for fact of alphabetic order, as if two have equal number of fixes, 
+        // it only updates if maxVal is less than theirs and we are going through in alphabetic orer
+        if (maxVal < user_tracker[sortedKeys[i]] || maxVal === user_tracker[sortedKeys[i]] && names[sortedKeys[i]] < names[currKey]){
+            maxVal = user_tracker[sortedKeys[i]];
+            currKey = sortedKeys[i];
+        }
+    }
+
+    // "If there wasn't any developer who fixed Documentation defects, the output would be null" this handles that
+    if (currKey == ""){
+        return null;   
+    }
+
+    console.log(currKey);
+    return String(currKey);  
+}
+
+function dfs(startNode, graph, visited){
+    let stack = []
+    let already_saw = new Set();
+
+    // store bug_id, index
+    stack.push([startNode, 0]);
+
+    // copied from dfs from activity 5
+    while (stack.length != 0){
+        let [current, index] = stack.pop();
+
+        // document node as having been visited now, will not look in future
+        if (!visited.has(current)){
+            visited.add(current);
+            // already saw is on current path, this is important for cycles
+            already_saw.add(current);
+        }
+
+        // this is "blocks"
+        let children = graph[current];
+
+        // going through block vals
+        if (index < children.length){
+            stack.push([current, index + 1]);
+            let child = children[index];
+            if (!visited.has(child)){
+                // were going to now move onto child or new block to start looking for dependancy
+                stack.push([child, 0]);
+            }
+            else if (already_saw.has(child)){
+                // this means child is already been seen and there is a cycle
+                return true;
+            }
+        }
+        else {
+            // have to remove children to keep going, dont want to get false cycles
+            already_saw.delete(current);
+        }
+    }
+    return false;
 }
 
 function query6(defects){
-    /* Your implementation here */
-    return /*...*/;    
+    // circular dependency check
+    let dependancy = false;
+    let graph = {};
+
+    // filling up graph with bug_id : blocks []
+    defects.forEach(defect => {
+        graph[defect.bug_id] = defect.blocks || [];
+    });
+
+    // need to keep track of visited nodes (bug id in this case)
+    let visited = new Set();
+
+    // go through all defects and now check for dependacy of them
+    for (let i = 0; i < defects.length; i++){
+        if (!visited.has(defects[i].bug_id)){
+            if (dfs(defects[i].bug_id, graph, visited)){
+                dependancy = true;
+            }
+        }
+    }
+    console.log(dependancy);
+    return dependancy;
 }
 
 
