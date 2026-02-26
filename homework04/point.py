bf5ee4bd7495ef2058a12699f8c8ed09444b7599
Noah Thompson <nthomps7@nd.edu>
2026-02-26 13:43:37 -0500

    point.py

--- homework04/point.py
+++ homework04/point.py
@@ -12,27 +12,80 @@ class Point:
         print(f"({self.x}, {self.y})")
     
     def distance(p1, p2):
+        # get points
         x2  = p2.x
         x1 = p1.x
         y2 = p2.y
         y1 = p1.y
         dis = ((x2-x1)**2 + (y2-y1)**2)**(1/2)
-        print(f"The points distance is {dis}")
+        return dis
 
     # check p1 > p2
     def __gt__(p1, p2):
-        x2  = p2.x
-        y2 = p2.y
+        # get points
+        zero = Point(0,0)
 
-        x1 = p1.x
-        y1 = p1.y
-        dist_p1 = ((0-x1)**2 + (0-y1)**2)**(1/2)
-        dist_p2 = ((x2-0)**2 + (y2-0)**2)**(1/2)
+        #get distances from origin
+        dist_p1 = Point.distance(p1, zero)
+        dist_p2 = Point.distance(p2, zero)
         if dist_p2 < dist_p1:
             return True
         return False
+    
+    # check P1 >=P2
+    def __ge__(p1, p2):
+        # get points
+        zero = Point(0,0)
 
+        #get distances from origin
+        dist_p1 = Point.distance(p1, zero)
+        dist_p2 = Point.distance(p2, zero)
+        if dist_p2 <= dist_p1:
+            return True
+        return False
+    
+    def __eq__(p1, p2):
+        # get points
+        zero = Point(0,0)
+
+        #get distances from origin
+        dist_p1 = Point.distance(p1, zero)
+        dist_p2 = Point.distance(p2, zero)
+        if dist_p2 == dist_p1:
+            return True
+        return False
+
+    # compare distance P1 < P2
+    def __lt__(p1, p2):
+        # get points
+        zero = Point(0,0)
+
+        #get distances from origin
+        dist_p1 = Point.distance(p1, zero)
+        dist_p2 = Point.distance(p2, zero)
+
+        if dist_p2 > dist_p1:
+            return True
+        return False
+    
+    # compare P1 <= P2
+    def __le__(p1, p2):
+        # get points
+        zero = Point(0,0)
+
+        #get distances from origin
+        dist_p1 = Point.distance(p1, zero)
+        dist_p2 = Point.distance(p2, zero)
+
+        if dist_p2 >= dist_p1:
+            return True
+        return False
+    
+# Example outputs
 p1 = Point(2,3)  
 p2 = Point(-3,1) 
 p3 = Point(-2,-3)
 print(p1 > p2)
+print(p1 == p2)
+print(p1 < p2)
+print(p1 == p3)
