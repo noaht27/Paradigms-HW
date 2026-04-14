8631f6bd7213b45dce679066fb52c55b5ac49e50
Noah Thompson <nthomps7@nd.edu>
2026-04-14 10:45:10 -0400

    started

--- homework08/Homework8.java
+++ homework08/Homework8.java
@@ -2,3 +2,15 @@
 // 14 April 2026
 // Homework8.java
 // Checking words for correct position at the correct index
+
+public class Homework8{
+    public boolean check(char[] correctPositions, String word) {
+        /* your solution goes here */
+        System.out.println("Function inside is working!");
+        return true;
+    }
+
+    public static void main(String[] args) {
+            System.out.println("Hello World");
+    }
+}
