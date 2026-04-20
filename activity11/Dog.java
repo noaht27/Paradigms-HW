91a1138af3cc69d6000aaaadfbedf95eb8aeb536
Noah Thompson <nthomps7@nd.edu>
2026-04-20 10:22:51 -0400

    done

--- activity11/Dog.java
+++ activity11/Dog.java
@@ -1,3 +1,15 @@
 // Noah Thompson
 // 20 April 2026
 // Dog.java
+
+public class Dog extends Mammal implements Animal{
+
+    public Dog(String name){
+        super(name);
+    }
+
+    @Override
+    public void makeSound(){
+        System.out.println("Bark");
+    }
+}
--- activity11/Mammal.java
+++ activity11/Mammal.java
@@ -1,3 +1,15 @@
 // Noah Thompson
 // 20 April 2026
 // Mammal.java
+
+public abstract class Mammal{
+    private String name;
+
+    public Mammal(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+}
