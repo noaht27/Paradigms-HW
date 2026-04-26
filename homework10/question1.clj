06ff930947dfca031f40643ae0da2357a24a822f
Noah Thompson <nthomps7@nd.edu>
2026-04-26 15:23:21 -0400

    q1 done

--- homework10/question1.clj
+++ homework10/question1.clj
@@ -2,3 +2,19 @@
 ;; 26 April 2026
 ;; question1.clj
 
+;; getting command line argument
+(def arg (Integer/parseInt (first *command-line-args*)))
+
+;; function that squares numbers
+(defn square [x] (* x x))
+
+;; range of numbers 1 - n
+(def ran (range 1 (+ arg 1)))
+
+;; use map to generate square of numbers
+(doseq [x (map square ran)]
+    (printf "%d\n" x))
+
+;; using reduce to get the sum
+(def sum (reduce + (map square ran)))
+(printf "Sum = %d\n" sum)
